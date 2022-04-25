f32e17b4ed4e714693f078f78e399205092c9808
thingineer <dlaudwls1203@gmail.com>
2022-04-25 19:48:03 +0900

    today's commit

---
+++ venv/1260.py
@@ -0,0 +1,45 @@
+#DFS BFS
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+def bfs(graph, v, visited):
+    q = deque([v])
+    visited[v] = True
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True
+
+#today's commit
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+n, m, v = map(int, input().split())
+
+visited = [False] * (n + 1)
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n + 1):
+    graph[i].sort()
+
+dfs(graph, v, visited)
+print()
+visited = [False] * (n + 1)
+bfs(graph, v, visited)
